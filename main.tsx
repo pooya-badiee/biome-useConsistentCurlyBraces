d99aa45e584f87a5ccc6a1ac6c04f59a11fe0971
pooya <pooya.badiee76@gmail.com>
2025-03-16 12:34:14 -0700

    init

---
+++ main.tsx
@@ -0,0 +1,12 @@
+
+const jsx = (
+  <div>
+    <div>
+      It looks like we do not deliver to this region. See our shipping details
+      <div>
+        here
+      </div>{' '}
+      or contact our Customer Care team for more information
+    </div>
+  </div>
+)
