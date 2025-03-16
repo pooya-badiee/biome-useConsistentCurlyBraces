7c1bfb4d99da907fe9b86afb874e075caaf851c2
pooya <pooya.badiee76@gmail.com>
2025-03-16 12:45:04 -0700

    recreate issue

--- main.tsx
+++ main.tsx
@@ -1,12 +1,20 @@
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const View: any = 'div'
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const Text: any = 'div'
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const Link: any = 'div'
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const shop: any = 'div'
 
 const jsx = (
-  <div>
-    <div>
+  <View>
+    <Text size="base" appearance="critical">
       It looks like we do not deliver to this region. See our shipping details
-      <div>
+      <Link appearance="monochrome" to={`${shop.storefrontUrl}'pages/shipping-and-returns`} external>
         here
-      </div>{' '}
+      </Link>{' '}
       or contact our Customer Care team for more information
-    </div>
-  </div>
+    </Text>
+  </View>
 )
